8a05becb7fae4b016d2584a0e36121af93ecc692
isaacbrds <isaac.brigido@gmail.com>
2022-06-22 11:05:01 -0300

    Aula 2.3 Criando o Jogo

---
+++ adivinhacao.py
@@ -0,0 +1,7 @@
+print("*********************************")
+print('Bem vindo ao jogo de Adivinhação!')
+print("*********************************")
+
+numero_secreto = 45
+
+int(input('Digite seu chute: '))
