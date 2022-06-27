f5faea4c02993c83557308a715251d8c780dc271
isaacbrds <isaacbrds>
2022-06-27 14:20:39 -0300

    Aula 7.2 Adicionando pontuaçao ao jogo

--- adivinhacao.py
+++ adivinhacao.py
@@ -5,8 +5,9 @@ print('Bem vindo ao jogo de Adivinhação!')
 print("*********************************")
 
 
-numero_secreto = random.randrange(1,101)
+numero_secreto       = random.randrange(1,101)
 numero_de_tentativas = 0
+pontuacao            = 1000
 
 print('Qual o nível de dificuldade? ')
 print('(1) Fácil (2) Médio (3) Difícil ')
@@ -21,6 +22,7 @@ else:
 
 for rodada in range(1, numero_de_tentativas + 1):
   print(f'Tentativa {rodada} de {numero_de_tentativas}')
+  
   chute = int(input('Digite seu chute: '))
 
   print(f'Você digitou {chute}')
@@ -32,11 +34,14 @@ for rodada in range(1, numero_de_tentativas + 1):
   menor = chute < numero_secreto
 
   if(acertou):
-    print("Você acertou!!")
+    print(f"Você acertou!! e fez {pontuacao}!! ")
     break
   elif(maior):
     print("Você errou, o chute foi maior que o número")
   elif(menor):
     print("Você errou, o chute foi menor que o número")
-  
+  pontos_perdidos = abs(numero_de_tentativas - chute)
+  pontuacao -= pontos_perdidos
   rodada += 1
+
+print('Fim de Jogo!!!!')
